cbe8f9c616764d76578e8cf6dd0522286c2f5921
Kamil Zak <kamilzak00@gmail.com>
2024-06-06 14:27:39 +0100

    feat: add hot reload & factory settings to uvicorn

--- backend/asgi.py
+++ backend/asgi.py
@@ -5,5 +5,12 @@ from api import create_app
 
 app = create_app()
 
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    uvicorn.run(
+        "api:create_app",
+        host="127.0.0.1",
+        port=5000,
+        reload=app.state.config.DEBUG,
+        factory=True,
+    )
