0bab0300b521eefd40219b0e4e2171313bd78078
Kamil Zak <kamilzak00@gmail.com>
2024-07-05 14:59:49 +0100

    feat: add document title to calendar page and functionality to logout button

--- frontend/src/components/common/Calendar/Sidebar.tsx
+++ frontend/src/components/common/Calendar/Sidebar.tsx
@@ -1,10 +1,32 @@
+import { useAuth } from "@/AuthProvider";
+import { userService } from "@/services/UserService";
+
 import {
   CalendarDaysIcon,
   Cog6ToothIcon,
   ArrowLeftStartOnRectangleIcon,
 } from "@heroicons/react/24/outline";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const { updateAuthentication } = useAuth();
+
+  const logout = async () => {
+    try {
+      await userService.logout();
+
+      toast.success("You have successfully been logged out!");
+
+      updateAuthentication(false);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      toast.error("Something went wrong when logging out.");
+    }
+  };
+
   return (
     <nav className="h-screen w-[12.5%] flex flex-col justify-between border-r-[1px] border-gray-300">
       <div className="flex flex-col">
@@ -27,7 +49,10 @@ const Sidebar = () => {
           <h1 className="font-bold">John Doe</h1>
           <h2 className="font-light text-sm">john@doe.com</h2>
         </div>
-        <ArrowLeftStartOnRectangleIcon className="w-6 h-6" />
+        <ArrowLeftStartOnRectangleIcon
+          className="w-6 h-6 hover:cursor-pointer"
+          onClick={() => logout()}
+        />
       </div>
     </nav>
   );
--- frontend/src/components/pages/Calendar/CalendarPage.tsx
+++ frontend/src/components/pages/Calendar/CalendarPage.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
+
 import Sidebar from "@/components/common/Calendar/Sidebar";
 import Calendar from "@/components/common/Calendar/Calendar";
 
 const CalendarPage = () => {
+  useEffect(() => {
+    document.title = "allocate — Calendar";
+  }, []);
+
   return (
     <div className="flex flex-row h-screen">
       <Sidebar />
