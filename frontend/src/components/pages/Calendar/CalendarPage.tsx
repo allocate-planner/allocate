c97a530a98da570ac6903bb7a3700eb18e0de98a
Kamil Zak <kamilzak00@gmail.com>
2024-07-05 15:19:34 +0100

    feat: add /calendar behind authentication

--- frontend/src/components/pages/Calendar/CalendarPage.tsx
+++ frontend/src/components/pages/Calendar/CalendarPage.tsx
@@ -2,11 +2,20 @@ import { useEffect } from "react";
 
 import Sidebar from "@/components/common/Calendar/Sidebar";
 import Calendar from "@/components/common/Calendar/Calendar";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/AuthProvider";
 
 const CalendarPage = () => {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
+
   useEffect(() => {
     document.title = "allocate — Calendar";
-  }, []);
+
+    if (!isAuthenticated) {
+      navigate("/");
+    }
+  }, [navigate, isAuthenticated]);
 
   return (
     <div className="flex flex-row h-screen">
