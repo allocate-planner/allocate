27a0d29842fa325d8b208735111316f149f65b56
Kamil Zak <kamilzak00@gmail.com>
2025-06-26 17:16:22 +0100

    fix: add default error page

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -4,23 +4,30 @@ import HomePage from "@/components/pages/Home/HomePage";
 import LoginPage from "@/components/pages/Login/LoginPage";
 import RegisterPage from "@/components/pages/Register/RegisterPage";
 import CalendarPage from "@/components/pages/Calendar/CalendarPage";
+import ErrorPage from "./components/pages/Error/ErrorPage";
+
+// TODO: Reorganise routes
 
 const Routes = () => {
   const router = createBrowserRouter([
     {
       path: "/",
+      errorElement: <ErrorPage />,
       element: <HomePage />,
     },
     {
       path: "/login",
+      errorElement: <ErrorPage />,
       element: <LoginPage />,
     },
     {
       path: "/register",
+      errorElement: <ErrorPage />,
       element: <RegisterPage />,
     },
     {
       path: "/calendar",
+      errorElement: <ErrorPage />,
       element: <CalendarPage />,
     },
   ]);
