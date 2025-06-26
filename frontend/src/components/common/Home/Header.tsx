2a1d8119adde2baedadb58161266bdd6e5852c9e
Kamil Zak <kamilzak00@gmail.com>
2025-06-26 17:19:09 +0100

    fix: show login button on mobile

--- frontend/src/components/common/Home/Header.tsx
+++ frontend/src/components/common/Home/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <header className="absolute inset-x-0 top-0 z-50">
       <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
-        <div className="flex lg:flex-1">
+        <div className="flex flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">allocate</span>
             <img alt="" src="logo.svg" className="h-8 w-auto" />
@@ -22,7 +22,7 @@ const Header = () => {
             </a>
           ))}
         </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+        <div className="flex flex-1 justify-end">
           <a href="/login">
             <Button className="rounded-md bg-indigo-600 px-6 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
               Log In
