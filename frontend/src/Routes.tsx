b67850da1a3fcfb140661934b57db6778a323c9d
Kamil Zak <kamilzak00@gmail.com>
2025-07-05 08:54:59 +0100

    feat: improve routes semantics

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -6,29 +6,26 @@ import RegisterPage from "@/components/pages/Register/RegisterPage";
 import CalendarPage from "@/components/pages/Calendar/CalendarPage";
 import ErrorPage from "./components/pages/Error/ErrorPage";
 
-// TODO: Reorganise routes
-
 const Routes = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       errorElement: <ErrorPage />,
       element: <HomePage />,
-    },
-    {
-      path: "/login",
-      errorElement: <ErrorPage />,
-      element: <LoginPage />,
-    },
-    {
-      path: "/register",
-      errorElement: <ErrorPage />,
-      element: <RegisterPage />,
-    },
-    {
-      path: "/calendar",
-      errorElement: <ErrorPage />,
-      element: <CalendarPage />,
+      children: [
+        {
+          path: "/login",
+          element: <LoginPage />,
+        },
+        {
+          path: "/register",
+          element: <RegisterPage />,
+        },
+        {
+          path: "/calendar",
+          element: <CalendarPage />,
+        },
+      ],
     },
   ]);
 
